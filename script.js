b1982e2bb32cbf5416420efb8b4bf3a6786c05d7
kambleps <125243027+kambleps@users.noreply.github.com>
2023-05-20 18:45:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function fibonacci(num) {
 // your code here
+	let n=num;
+	let first = 0;
+	let second = 1;
+	let next = 0;
+	for(let i=1;i<=n;i++)
+		{
+			next = first+second;
+			first=second;
+			second=next;
+		}
+	return next;
 }
 
 module.exports = fibonacci;
